cab551d6562624e4632cbbc8eb27f5392e3aa8ca
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-06 14:33:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,8 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   @@all = []
-  attr_accessor :name, :grade, :id
+  attr_reader :id
+  attr_accessor :name, :grade
 
   def initialize(name, grade)
     @id = nil
