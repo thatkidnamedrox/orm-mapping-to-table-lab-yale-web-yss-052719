3ed1375dde26320be64f2ae556a7447a2847113c
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-06 14:27:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,9 @@ class Student
    def self.create_table
      sql = <<-SQL
      CREATE TABLE IF NOT EXISTS students (
-
+       id INTEGER PRIMARY KEY,
+       name TEXT,
+       grade INTEGER
      )
      SQL
      DB[:conn].execute(sql)
