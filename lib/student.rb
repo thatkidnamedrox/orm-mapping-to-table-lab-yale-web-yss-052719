e91669a6bc95a1b51198a2ace9ded57ee857fa1c
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-06 14:27:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn]
   @@all = []
   def initialize(attributes)
     attributes.each {|key, value| self.send("#{key}=", value)}
@@ -10,7 +10,7 @@ class Student
    def self.create_table
      sql = <<-SQL
      CREATE TABLE IF NOT EXISTS students (
-       
+
      )
      SQL
      DB[:conn].execute(sql)
