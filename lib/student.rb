78e48092b60a1fc30384ee3664ac6e7f91e5a8aa
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-06 14:38:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,8 +35,10 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.create(name, grade)
-    student = Student.new(name, grade)
+  def self.create(attributes)
+    name = attributes[:name]
+    grade = attributes[:grade]
+    student = Student.new(name:, grade)
     student.save
     student
   end
