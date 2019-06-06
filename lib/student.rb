4fa0e496e2f30f9acdf895eb750c1045eb0dd8be
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-06 14:28:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,14 +8,15 @@ class Student
     attributes.each {|key, value| self.send("#{key}=", value)}
     @@all << self
   end
-   def self.create_table
-     sql = <<-SQL
+  def self.create_table
+    sql = <<-SQL
      CREATE TABLE IF NOT EXISTS students (
        id INTEGER PRIMARY KEY,
        name TEXT,
        grade INTEGER
      )
      SQL
-     DB[:conn].execute(sql)
-   end
+    DB[:conn].execute(sql)
+  end
+
 end
